cf485290748200d40f2fb693affda72561e33fbc
janeosaur <janewie@gmail.com>
2017-04-30 22:11:03 -0700

    delete works

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -44,15 +44,29 @@ wineIndexController.$inject = ['$http'];
 function wineIndexController( $http ) {
   var vm = this;
   vm.wines = {};
+  vm.newWine = {};
   $http ({
     method: 'get',
     url: 'https://super-crud.herokuapp.com/wines'
   }).then(function successCallback(response) {
-    // console.log('get req successful. response data is ', response.data.wines)
     vm.wines = response.data.wines
   }, function errorCallback(response) {
     console.log('get req error ', response)
   });
+
+// post returns a server error...
+  vm.addWine = function () {
+    $http ({
+      method: 'POST',
+      url: 'https://super-crud.herokuapp.com/wines',
+      data: vm.newWine
+    }).then(function successCallback(response) {
+      console.log('post successful', response.data);
+      vm.wines.push(response.data);
+    }, function errorCallback(response) {
+      console.log('post error', response)
+    })
+  }
 }
 
 wineShowController.$inject = ['$http', '$routeParams', '$location'];
@@ -68,4 +82,16 @@ function wineShowController( $http, $routeParams, $location ) {
   }, function errorCallback(response) {
     console.log('error', response)
   });
+
+  vm.deleteWine = function (wine) {
+    $http({
+      method: 'delete',
+      url: 'https://super-crud.herokuapp.com/wines/' + wine._id // or .id?
+    }).then(function successCallback(response) {
+      console.log('delete success ', response);
+      $location.path('/');
+    }, function errorCallback(response) {
+      console.log('delete error ', response)
+    })
+  }
 }
